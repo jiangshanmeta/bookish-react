92c90df07d1992db00f8b834b5be1ed12d04c30b
jiangshanmeta <540118044@qq.com>
2024-06-01 14:45:03 +0800

    feat: make the first e2e test pass

---
+++ cypress/e2e/bookish.spec.cy.ts
@@ -0,0 +1,6 @@
+describe('Bookish Application', () => {
+  it('Visit the bookish', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get("h2[data-test='heading']").contains("Bookish")
+  })
+})
